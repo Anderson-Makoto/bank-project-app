7c7a93303c49de47b91f759148250e4fa7b53637
Anderson-Makoto <anderson.shinoda1@gmail.com>
2021-11-08 02:42:10 -0300

    implement customer home screen

---
+++ bankProjectApp/src/sharedComponents/drawerContent/drawerContent.style.js
@@ -0,0 +1,32 @@
+import { StyleSheet } from "react-native"
+import { colors } from "../../helpers/constants"
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: "column",
+        flex: 1,
+        alignItems: "center"
+    },
+    menuTitle: {
+        flex: 1,
+        width: "100%",
+        backgroundColor: colors.BLUE_2,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    menuTitleText: {
+        fontSize: 30,
+        color: colors.WHITE,
+    },
+    content: {
+        flex: 6,
+        backgroundColor: colors.BLUE_1,
+        width: "100%"
+    },
+    itemLabel: {
+        color: colors.WHITE,
+        fontSize: 10
+    }
+})
+
+export default styles
---
+++ bankProjectApp/src/sharedComponents/drawerTitle/drawerTitle.style.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native"
+import { colors } from "../../helpers/constants"
+
+const styles = StyleSheet.create({
+    container: {
+        height: "10%",
+        width: "100%",
+        flexDirection: "row",
+        backgroundColor: "red"
+    },
+    iconView: {
+        height: "100%",
+        width: "20%",
+        backgroundColor: "red"
+    }
+})
